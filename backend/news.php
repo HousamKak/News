1d33708d6ba09d34a125535bbcf6594f37f369bb
“Housam <housam.kak20@gmail.com>
2022-09-23 21:18:38 +0300

    established connection with database

--- backend/news.php
+++ backend/news.php
@@ -2,5 +2,18 @@
 
 include("connection.php");
 
+// getting the query result
+$query = $mysqli->prepare("SELECT title,text FROM news");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
 
 ?>
